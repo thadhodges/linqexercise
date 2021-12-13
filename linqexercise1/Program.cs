e386bc42c859df3ca61dc3a28ed8ab86546d4e7b
Thad <thadisfree@gmail.com>
2021-12-13 12:50:23 -0600

    exercise1 complete

--- linqexercise1/Program.cs
+++ linqexercise1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var nameList = new List<string>() { "frogger", "pac-man", "galactica", "flapdap", "shopfunlogname"};
+
+var sortByLength = nameList.OrderBy(x => x.Length);
+Console.WriteLine("list by ascending order");
+foreach ( var item in sortByLength )
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine();
+Console.WriteLine("list by descending order");
+var descSort = nameList.OrderByDescending(x => x.Length);
+foreach ( var item in descSort )
+{
+    Console.WriteLine(item);
+}

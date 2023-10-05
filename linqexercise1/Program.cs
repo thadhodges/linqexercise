6e6b33ebc04605fe304ee9b87a5e991b6ed68fe1
thadhodges <77890659+thadhodges@users.noreply.github.com>
2023-10-05 17:38:39 -0500

    another commit

--- linqexercise1/Program.cs
+++ linqexercise1/Program.cs
@@ -1,41 +1,8 @@
 ﻿using static System.Console;
 
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var nameList = new List<string>() { "123", "123456", "12345678", "12", "12345678910", "12345" };
-
-        var sortByLength = nameList.OrderBy(x => x.Length);
-        Console.WriteLine("list by ascending order");
-        foreach (var item in sortByLength)
-        {
-            Console.WriteLine(item);
-        }
-        Console.WriteLine();
-        Console.WriteLine("list by descending order");
-        var descSort = nameList.OrderByDescending(x => x.Length);
-        foreach (var item in descSort)
-        {
-            Console.WriteLine(item);
-        }
-        System.Console.WriteLine();
-        System.Console.WriteLine(   );
-
-        //additional code testing
-        var nameListArrowStrength = new List<int> {11,21,31,41,51,61,71,81,91};
-        int number5 = nameListArrowStrength[4];
-        Random random = new Random();
 
 
-        Console.WriteLine($"Random selection from list: {nameListArrowStrength[random.Next(0, nameListArrowStrength.Count)]}");
-
-        //System.Console.WriteLine(number5);
-        WriteLine("Press any key to exit...");
-        ReadKey();
-        
-    }
-
-}
-        
+System.Console.WriteLine("hello");
+int number = 45;
+System.Console.WriteLine(number);
